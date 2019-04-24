ba451ebcc9a879d7602aad126d6c72af8507d81c
Gabri312 <gabriel.cmiel@htwg-konstanz.de>
2019-04-24 17:31:47 +0200

    +- Delete unnecessary files

--- ownership/ownership_exercise/target/debug/deps/ownership_exercise-7255b36902b18528.d
+++
@@ -1,5 +0,0 @@
-/home/gabriel/IdeaProjects/pir-ss19-homeworks-grp7/ownership_exercise/target/debug/deps/ownership_exercise-7255b36902b18528: src/main.rs
-
-/home/gabriel/IdeaProjects/pir-ss19-homeworks-grp7/ownership_exercise/target/debug/deps/ownership_exercise-7255b36902b18528.d: src/main.rs
-
-src/main.rs:
--- ownership/ownership_exercise_solved/target/debug/deps/ownership_exercise_solved-88ddb753f68eb66e.d
+++
@@ -1,5 +0,0 @@
-/home/gabriel/IdeaProjects/pir-ss19-homeworks-grp7/ownership_exercise_solved/target/debug/deps/ownership_exercise_solved-88ddb753f68eb66e: src/main.rs
-
-/home/gabriel/IdeaProjects/pir-ss19-homeworks-grp7/ownership_exercise_solved/target/debug/deps/ownership_exercise_solved-88ddb753f68eb66e.d: src/main.rs
-
-src/main.rs:
