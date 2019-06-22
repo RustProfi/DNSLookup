a3e5820dd3f2e46de24a62b8b70b5b0c551226d1
Gabri312 <gabriel.cmiel@htwg-konstanz.de>
2019-06-22 19:18:16 +0200

    ++ fnkt to check if argument is ip ++ argparser

--- nslookup/src/main.rs
+++ nslookup/src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 && &args[1] != "-help" {
+
+        println!("{}", check_ip(&args[1]))
+    } else {
+        println!("Usage is: nslookup [Host Name] | [Host IP] | -help");
+        println!("nslookup foo.bar.com (Returns IP Address for Host Name)");
+        println!("nslookup 123.123.123.123 (Returns Host Name for Address)");
+        println!("nslookup -help (Returns this Help Message)");
+    }
+}
+
+fn check_ip(ip: &str) -> bool {
+    if ip.split(".").count() == 4 {
+        ip.split(".").all(|s| s.parse::<i32>().is_ok())
+    } else {
+        false
+    }
 }
