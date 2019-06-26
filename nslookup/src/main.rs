37493673a7d926fe4bf8c02873c523d971e15b15
Gabri312 <gabriel.cmiel@htwg-konstanz.de>
2019-06-26 13:22:58 +0200

    ++ get other addresses from host name

--- nslookup/src/main.rs
+++ nslookup/src/main.rs
@@ -1,10 +1,25 @@
 use std::env;
+use std::net::{ToSocketAddrs, SocketAddr, UdpSocket};
+use std::vec::IntoIter;
+use std::io::Error;
 
 fn main() {
+    let sock = UdpSocket::bind("0.0.0.0:0").unwrap();
     let args: Vec<String> = env::args().collect();
     if args.len() == 2 && &args[1] != "-help" {
+        if check_ip(&args[1]) {
+            println!("reverse lookup")
+        } else {
+            match get_address(&args[1]) {
+                Ok(iter) => {
+                    for x in iter {
+                        x //sock.send_to(,x)
+                    }
+                }
+                Err(e) => println!("{:?}", e),
+            };
 
-        println!("{}", check_ip(&args[1]))
+        }
     } else {
         println!("Usage is: nslookup [Host Name] | [Host IP] | -help");
         println!("nslookup foo.bar.com (Returns IP Address for Host Name)");
@@ -12,6 +27,13 @@ fn main() {
         println!("nslookup -help (Returns this Help Message)");
     }
 }
+
+fn get_address(domain: &str) -> Result<IntoIter<SocketAddr>, Error> {
+    format!("{}:53", domain).to_socket_addrs()
+}
+
+
+
 
 fn check_ip(ip: &str) -> bool {
     if ip.split(".").count() == 4 {
