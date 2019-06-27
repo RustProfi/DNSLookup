f1c2602791059d8a67a6d5db10f3cb7bd8a0064b
Gabri312 <gabriel.cmiel@htwg-konstanz.de>
2019-06-27 17:59:56 +0200

    ++ decoder from hex to bytes ++ encoder from bytes to hex ++ Header for our message

--- nslookup/src/main.rs
+++ nslookup/src/main.rs
@@ -2,6 +2,24 @@ use std::env;
 use std::net::{ToSocketAddrs, SocketAddr, UdpSocket};
 use std::vec::IntoIter;
 use std::io::Error;
+use std::str;
+use std::num::ParseIntError;
+use std::fmt::Write;
+use std::num;
+
+struct Header {
+    /// Header for our query
+    id: u16,
+    qr: bool,
+    opcode: bool,
+}
+
+impl Header {
+    fn new(id: u16, qr: bool, opcode: bool) -> String {
+        let bin = format!("{}000{}00100000000", qr, opcode);
+        format!("{:0>4x}",id)
+    }
+}
 
 fn main() {
     let sock = UdpSocket::bind("0.0.0.0:0").unwrap();
@@ -10,15 +28,10 @@ fn main() {
         if check_ip(&args[1]) {
             println!("reverse lookup")
         } else {
-            match get_address(&args[1]) {
-                Ok(iter) => {
-                    for x in iter {
-                        x //sock.send_to(,x)
-                    }
-                }
-                Err(e) => println!("{:?}", e),
-            };
-
+            let mut buf = [0u8;4096];
+            sock.send_to(&message[..],"141.37.11.1:53");
+            let (amt, src) = sock.recv_from(&mut buf).unwrap();
+            println!("{:?}", Vec::from(&buf[0..amt]));
         }
     } else {
         println!("Usage is: nslookup [Host Name] | [Host IP] | -help");
@@ -28,13 +41,30 @@ fn main() {
     }
 }
 
-fn get_address(domain: &str) -> Result<IntoIter<SocketAddr>, Error> {
-    format!("{}:53", domain).to_socket_addrs()
+/// Returns a u8 vector on success else an Error
+/// # Arguments
+/// * `hex` - a hex &str that we want to parse
+fn decode(hex: &str) -> Result<Vec<u8>,ParseIntError> {
+    (0..hex.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&hex[i..i + 2], 16))
+        .collect()
 }
 
+/// Returns hex of a u8
+/// # Arguments
+/// * `bytes` - bytes that we want to parse
+fn encode(bytes: &[u8]) -> String {
+    let mut s = String::with_capacity(bytes.len() * 2);
+    for &b in bytes {
+        write!(&mut s, "{:02x}", b);
+    }
+    s
+}
 
-
-
+/// Returns true if argument is an IP
+/// # Arguments
+/// * `bytes` - bytes that we want to parse
 fn check_ip(ip: &str) -> bool {
     if ip.split(".").count() == 4 {
         ip.split(".").all(|s| s.parse::<i32>().is_ok())
