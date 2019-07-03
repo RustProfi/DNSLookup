6a4d9b696fe119676fac5e56205fee4a4e23c41d
Gabri312 <gabriel.cmiel@htwg-konstanz.de>
2019-07-03 20:25:54 +0200

    ++ question test

--- nslookup/tests/integration_test.rs
+++ nslookup/tests/integration_test.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 extern crate nslookup;
 use nslookup::qtype::Qtype;
+use nslookup::question;
+use nslookup::question::{Question, Header};
 
 #[test]
 fn test_qtype() {
@@ -10,4 +12,12 @@ fn test_qtype() {
     assert_eq!(Qtype::get_qtype(1).unwrap().value(), Qtype::A.value());
     assert_eq!(Qtype::get_qtype(28).unwrap().value(), Qtype::AAAA.value());
     assert_eq!(Qtype::get_qtype(5).unwrap().value(), Qtype::CNAME.value());
+}
+
+#[test]
+fn test_question() {
+    let header = Header::new_message(1,false, false);
+    assert_eq!(header.unwrap(),b"\x00\x01\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00");
+    let header2 = Header::new_message(2,false, false);
+    assert_eq!(Question::new_question(header2.unwrap(),"example.com", Qtype::A), b"\x00\x02\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x07\x65\x78\x61\x6d\x70\x6c\x65\x03\x63\x6f\x6d\x00\x00\x01\x00\x01")
 }
